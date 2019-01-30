fd94985a6ffd191e71d7d7c18e2cf7286e8daa1b
Diamondgal <michwray74@gmail.com>
2019-01-30 16:21:41 -0500

    else if statements for game

--- livelife.js
+++ livelife.js
@@ -1,5 +1,5 @@
 alert ("Live Life Game");
-let choice=prompt("Hey Player! This is the beginning of your self-improvement journey to Enlightenment. To get started, you need to choose one letter from a through f.")
+let choice=prompt("Hey Player! This is the beginning of your self-improvement journey to Enlightenment. To get started, hit the letter a.")
 
 
 // let a ="Dalai Lama";
@@ -10,7 +10,7 @@ let choice=prompt("Hey Player! This is the beginning of your self-improvement jo
 // let f ="Spiderman comicbook";
 
 let characOne="Your challenge is to meet the Dalai Lama";
-let characTwo="Your challenge is to meet the potential love of your life";
+let characTwo="Your next challenge is that you bump into a stranger on the pavement while you have your head buried into your cellphone, this could be love of your life";
 // let characThree="Stray dog";
 // let characFour="Lost child";
 // let characFive="Hot dog vendor";
@@ -24,18 +24,67 @@ if (choice === "a") {
     let chalOneAvoid="Smile and wave";
     let chalOneIgnore="Walk on by";
     let chalOneConfront="Stop and say hi";
-    choice = prompt("Welcome to your first challenge, you are with the Dalai Lama in a Zen Garden. You can make one of three choices: \n b =" + chalOneAvoid + "\n  a= " + chalOneIgnore + "\n  c="+ chalOneConfront );
-    if (choice === "b") {
+    choice = prompt("Welcome to your challenge, you are with the Dalai Lama in a Zen Garden. You can make one of three choices: \na= " + chalOneIgnore + " \nb =" + chalOneAvoid + "  \nc=" + chalOneConfront );
+
+    while (choice !=="c") {
+    
+
+    if (choice === "b"){ 
         alert("Did you really think you would get enlightened through the power of osmosis?");
     }
+
+    
     else if (choice ==="a") {
         alert("blah blah")
     }
+ 
+    if (choice !=="c"){
+        choice = prompt("Try again: \na= " + chalOneIgnore + 
+        " \nb =" + chalOneAvoid + "  \nc=" + chalOneConfront); 
+        
+    }
+    } 
+    if (choice ==="c"){
+    alert("Yes! Regardless of what you believe the quote about his Nobel Piece Prize Win")
+    }
+    }
 
-}
-else if (choice ==="b") {
-    alert(characTwo);
-}
+alert(characTwo);{
+    let chalOneAvoid="Apologise profusely and keep moving";
+    let chalOneIgnore="Snarl in irritation, $`watch where you walk!`a";
+    let chalOneConfront="Smile and say hi";
+    choice = prompt("You look up into their eyes and can make one of three choices: \na= " + chalOneIgnore + " \nb =" + chalOneAvoid + "  \nc=" + chalOneConfront );
+
+    while (choice !=="c") {
+    
+
+    if (choice === "b"){ 
+        alert("hey Grumpy - what's the rush?  Ever stopped to ask yourself how many new friends you made in a day?  Guess you keep having the same answer, right? None!");
+    }
+
+    
+    else if (choice ==="a") {
+        alert("Sure you can keep on walking for the rest of your life and apologise for bumping into strangers, keep waling to your grave, old and lonely")
+    }
+ 
+    if (choice !=="c"){
+        choice = prompt("Try again: \na= " + chalOneIgnore + 
+        " \nb =" + chalOneAvoid + "  \nc=" + chalOneConfront); 
+        
+    }
+    } 
+    if (choice ==="c"){
+    alert("Yes! Regardless of what you believe the quote about his Nobel Piece Prize Win")
+    }
+    }
+
+
+
+
+// }
+// else if (choice ==="b") {
+//     alert(characTwo);
+// }
 
 
 
