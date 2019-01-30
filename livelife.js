b6e71cd7b7306c6d89386ef93b3296cad229d4fd
Diamondgal <michwray74@gmail.com>
2019-01-30 14:01:00 -0500

    init commit

---
+++ livelife.js
@@ -0,0 +1,93 @@
+alert ("Live Life Game");
+let choice=prompt("Hey Player! This is the beginning of your self-improvement journey to Enlightenment. To get started, you need to choose one letter from a through f.")
+
+
+// let a ="Dalai Lama";
+// let b ="Potential love of your life";
+// let c ="Stray dog";
+// let d ="Lost child";
+// let e ="Hot dog vendor";
+// let f ="Spiderman comicbook";
+
+let characOne="Your challenge is to meet the Dalai Lama";
+let characTwo="Your challenge is to meet the potential love of your life";
+// let characThree="Stray dog";
+// let characFour="Lost child";
+// let characFive="Hot dog vendor";
+// let characSix="Spiderman comicbook";
+
+
+
+
+if (choice === "a") { 
+    alert(characOne);
+    let chalOneAvoid="Smile and wave";
+    let chalOneIgnore="Walk on by";
+    let chalOneConfront="Stop and say hi";
+    choice = prompt("Welcome to your first challenge, you are with the Dalai Lama in a Zen Garden. You can make one of three choices: \n b =" + chalOneAvoid + "\n  a= " + chalOneIgnore + "\n  c="+ chalOneConfront );
+    if (choice === "b") {
+        alert("Did you really think you would get enlightened through the power of osmosis?");
+    }
+    else if (choice ==="a") {
+        alert("blah blah")
+    }
+
+}
+else if (choice ==="b") {
+    alert(characTwo);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (choice === b ){
+//     console.log (characTwo)
+// }
+// if (choice === c){
+//     console.log (characThree)
+// }
+// if (choice === d){
+//     console.log (characFour)
+// }
+// if (choice === e){
+//     console.log (characFive)
+// }
+// if (choice === f){
+//     console.log (characSix)
+// }
+// else if ((choice !=a) || (choice !=b) || (choice !=c) || (choice !=d) || (choice !=e) || (choice !=f))
+// choice=prompt (a,b,c,d,e,f) 
+
+
+
+// choice =prompt
+
+
+
+// let settingOne="Zen Garden";
+// let challengeOne="Welcome to your first challenge, you are about to meet the Dalai Lama in a Zen Garden. You can make one of three choices";
+// let chalOneAvoid="Smile and wave";
+// let chalOneIgnore="Walk on by";
+// let chalOneConfront="Stop and say hi";
+
+// choice=prompt(challengeOne);
+// choice
+// if (choice === characOne) {
+// console.log (chalOne)
+// }
+
+
+// if (characOne + settingOne + challengeOne === challengeOneAvoid) {
+//     console.log ("Did you really think you would get enlightened through the power of osmosis?")
+// }
